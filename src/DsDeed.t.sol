3268d3c5a2efb514e54dfd7bf0d0a52c67369c15
Brian McMichael <brian@brianmcmichael.com>
2020-06-25 11:42:34 -0400

    dapp init DsDeed

---
+++ src/DsDeed.t.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.5.15;
+
+import "ds-test/test.sol";
+
+import "./DsDeed.sol";
+
+contract DsDeedTest is DSTest {
+    DsDeed deed;
+
+    function setUp() public {
+        deed = new DsDeed();
+    }
+
+    function testFail_basic_sanity() public {
+        assertTrue(false);
+    }
+
+    function test_basic_sanity() public {
+        assertTrue(true);
+    }
+}

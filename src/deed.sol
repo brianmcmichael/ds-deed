342137b6c099f1f65ac275e8bc099296d1111373
Brian McMichael <brian@brianmcmichael.com>
2020-06-26 16:41:25 -0400

    Import ds-stop

--- src/deed.sol
+++ src/deed.sol
@@ -1,10 +1,21 @@
 pragma solidity >0.4.20;
 
+import "ds-stop/stop.sol";
+
 import "./base.sol";
 
-contract DSDeed is DSDeedBase {
+contract DSDeed is DSDeedBase, DSStop {
 
     constructor(string memory name, string memory symbol) DSDeedBase(name, symbol) public {}
 
+    event Mint(address indexed guy, uint256 nft);
+    event Burn(address indexed guy, uint256 nft);
 
+    function mint() public {
+        // TODO
+    }
+
+    function burn() public {
+        // TODO
+    }
 }

a7e390922b82980859bb17e620a1be6c105e0d2e
Brian McMichael <brian@brianmcmichael.com>
2020-06-26 15:25:31 -0400

    Base implementation (untested)

--- src/base.sol
+++ src/base.sol
@@ -21,21 +21,23 @@ import "erc721/erc721.sol";
 
 contract DSDeedBase is ERC721, ERC721Enumerable, ERC721Metadata {
 
-    string private _name;
-    string private _symbol;
+    string                                         private _name;
+    string                                         private _symbol;
 
-    mapping (uint256 => string) _uris;
+    mapping (uint256 => string)                    private _uris;
 
-    mapping (bytes4 => bool) private _interfaces;
+    mapping (bytes4 => bool)                       private _interfaces;
 
-    uint256[]                      private  _allDeeds;
-    mapping (address => uint256[]) private  _usrDeeds;
-    mapping (uint256 => Deed)      private  _deeds;
+    uint256[]                                      private _allDeeds;
+    mapping (address => uint256[])                 private _usrDeeds;
+    mapping (uint256 => Deed)                      private _deeds;
+    mapping (address => mapping (address => bool)) private _operators;
 
     struct Deed {
-        uint256  pos;
-        uint256 upos;
-        address  guy;
+        uint256      pos;
+        uint256     upos;
+        address      guy;
+        address approved;
     }
 
     constructor(string memory name, string memory symbol) public {
@@ -44,6 +46,16 @@ contract DSDeedBase is ERC721, ERC721Enumerable, ERC721Metadata {
         _addInterface(0x80ac58cd); // ERC721
         _addInterface(0x5b5e139f); // ERC721Metadata
         _addInterface(0x780e9d63); // ERC721Enumerable
+    }
+
+    modifier mine(uint256 nft) {
+        require(
+            _deeds[nft].guy == msg.sender ||
+            _deeds[nft].approved == msg.sender ||
+            _operators[_deeds[nft].guy][msg.sender],
+            "ds-deed-insufficient-approval"
+        );
+        _;
     }
 
     function name() public view returns (string) {
@@ -59,7 +71,7 @@ contract DSDeedBase is ERC721, ERC721Enumerable, ERC721Metadata {
     }
 
     function totalSupply() public view returns (uint256) {
-        return _allDeeds.length();
+        return _allDeeds.length;
     }
 
     function tokenByIndex(uint256 idx) public view returns (uint256) {
@@ -71,7 +83,14 @@ contract DSDeedBase is ERC721, ERC721Enumerable, ERC721Metadata {
     }
 
     function onERC721Received(address op, address src, uint256 nft, bytes what) external returns(bytes4) {
-        //TODO
+        return this.onERC721Received.selector;
+    }
+
+    function _isContract(address addr) private returns (bool) {
+        bytes32 codehash;
+        bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470; //EIP-1052
+        assembly { codehash := extcodehash(addr) }
+        return (codehash != accountHash && codehash != 0x0);
     }
 
     function supportsInterface(bytes4 interfaceID) external view returns (bool) {
@@ -91,30 +110,61 @@ contract DSDeedBase is ERC721, ERC721Enumerable, ERC721Metadata {
     }
 
     function safeTransferFrom(address src, address dst, uint256 nft, bytes what) public payable {
-        //TODO
+        _safeTransfer(src, dst, nft, data);
     }
 
     function safeTransferFrom(address src, address dst, uint256 nft) public payable {
-        //TODO
+        _safeTransfer(src, dst, nft, "");
     }
 
-    function transferFrom(address src, address dst, uint256 nft) public payable {
-        //TODO
+    function _safeTransfer(address src, address dst, uint256 nft, bytes data) internal {
+        transferFrom(src, dst, nft);
+        if (_isContract(dst)) {
+            bytes4 res = ERC721TokenReceiver(dst).onERC721Received(msg.sender, src, nft, data);
+            require(res == this.onERC721Received.selector, "ds-deed-invalid-token-receiver");
+        }
     }
 
-    function approve(address guy, uint256 nft) public payable returns (address) {
-        //TODO
+    function transferFrom(address src, address dst, uint256 nft) public payable mine(nft) {
+        require(src == _deeds[nft].guy, "ds-deed-src-not-valid");
+        require(dst != address(0), "ds-deed-unsafe-destination");
+        require(_deeds[nft].guy != address(0), "ds-deed-invalid-nft");
+        _upop(nft);
+        _upush(dst, nft);
+        emit Transfer(src, dst, nft);
+    }
+
+    function _upop(uint256 nft) internal {
+        uint256[] _udds = _usrDeeds[_deeds[nft].guy];
+        uint256   _uidx = _deeds[nft].upos;
+        uint256   _move = _udds[_udds.length - 1];
+        _udds[_uidx] = _move;
+        _deeds[nft].upos = _uidx;
+        _udds.pop();
+        _usrDeeds[_deeds[nft].guy] = _udds;
+    }
+
+    function _upush(address guy, uint256 nft) internal {
+        _deeds[nft].upos = _usrDeeds[nft].length;
+        _usrDeeds[guy].push(nft);
+        _deeds[nft].guy = guy;
+    }
+
+    function approve(address guy, uint256 nft) public payable mine(nft) returns (address) {
+        _deeds[nft].approval = guy;
+        emit Approval(msg.sender, guy, nft);
     }
 
     function setApprovalForAll(address op, bool ok) public {
-        //TODO
+        _operators[msg.sender][op] = ok;
+        emit ApprovalForAll(msg.sender, op, ok);
     }
 
     function getApproved(uint256 nft) public returns (address) {
-        //TODO
+        return _deeds[nft].approved;
     }
 
     function isApprovedForAll(address guy, address op) public view returns (bool) {
-        //TODO
+        return (_operators[guy] == op);
     }
 }

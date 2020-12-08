5bc117e5c205b95572748900a80fc3b6f195204f
Brian McMichael <brian@brianmcmichael.com>
2020-12-07 20:19:11 -0500

    ERC721 metadata docs

--- src/deed.t.sol
+++ src/deed.t.sol
@@ -75,15 +75,24 @@ contract DSDeedTest is DSTest {
 
 
     // ERC721 Metadata
+    /// title ERC-721 Non-Fungible Token Standard, optional metadata extension
+    /// dev See https://eips.ethereum.org/EIPS/eip-721
+    ///  Note: the ERC-165 identifier for this interface is 0x5b5e139f.
 
+    /// notice A descriptive name for a collection of NFTs in this contract
     function testName() public {
         assertEq(deed.name(), _name);
     }
 
+    /// notice An abbreviated name for NFTs in this contract
     function testSymbol() public {
         assertEq(deed.symbol(), _symb);
     }
 
+    /// notice A distinct Uniform Resource Identifier (URI) for a given asset.
+    /// dev Throws if `_tokenId` is not a valid NFT. URIs are defined in RFC
+    ///  3986. The URI may point to a JSON file that conforms to the "ERC721
+    ///  Metadata JSON Schema".
     function testTokenURI() public {
         deed.mint(_addr, _uri);  //setup
         deed.mint(_addr, "t2");

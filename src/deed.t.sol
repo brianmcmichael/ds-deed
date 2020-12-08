fb19e8a35b6019a4f365edd6719144ab1e883057
Brian McMichael <brian@brianmcmichael.com>
2020-12-07 20:45:57 -0500

    Test notes

--- src/deed.t.sol
+++ src/deed.t.sol
@@ -151,6 +151,96 @@ contract DSDeedTest is DSTest {
     }
 
 
+    /// notice Transfers the ownership of an NFT from one address to another address
+    /// dev Throws unless `msg.sender` is the current owner, an authorized
+    ///  operator, or the approved address for this NFT. Throws if `_from` is
+    ///  not the current owner. Throws if `_to` is the zero address. Throws if
+    ///  `_tokenId` is not a valid NFT. When transfer is complete, this function
+    ///  checks if `_to` is a smart contract (code size > 0). If so, it calls
+    ///  `onERC721Received` on `_to` and throws if the return value is not
+    ///  `bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"))`.
+    /// param _from The current owner of the NFT
+    /// param _to The new owner
+    /// param _tokenId The NFT to transfer
+    /// param data Additional data with no specified format, sent in call to `_to`
+    //function safeTransferFrom(address _from, address _to, uint256 _tokenId, bytes data) external payable;
+
+    /// notice Transfers the ownership of an NFT from one address to another address
+    /// dev This works identically to the other function with an extra data parameter,
+    ///  except this function just sets data to "".
+    /// param _from The current owner of the NFT
+    /// param _to The new owner
+    /// param _tokenId The NFT to transfer
+    //function safeTransferFrom(address _from, address _to, uint256 _tokenId) external payable;
+
+    /// notice Transfer ownership of an NFT -- THE CALLER IS RESPONSIBLE
+    ///  TO CONFIRM THAT `_to` IS CAPABLE OF RECEIVING NFTS OR ELSE
+    ///  THEY MAY BE PERMANENTLY LOST
+    /// dev Throws unless `msg.sender` is the current owner, an authorized
+    ///  operator, or the approved address for this NFT. Throws if `_from` is
+    ///  not the current owner. Throws if `_to` is the zero address. Throws if
+    ///  `_tokenId` is not a valid NFT.
+    /// param _from The current owner of the NFT
+    /// param _to The new owner
+    /// param _tokenId The NFT to transfer
+    //function transferFrom(address _from, address _to, uint256 _tokenId) external payable;
+
+    /// notice Change or reaffirm the approved address for an NFT
+    /// dev The zero address indicates there is no approved address.
+    ///  Throws unless `msg.sender` is the current NFT owner, or an authorized
+    ///  operator of the current owner.
+    /// param _approved The new approved NFT controller
+    /// param _tokenId The NFT to approve
+    //function approve(address _approved, uint256 _tokenId) external payable;
+
+    /// notice Enable or disable approval for a third party ("operator") to manage
+    ///  all of `msg.sender`'s assets
+    /// dev Emits the ApprovalForAll event. The contract MUST allow
+    ///  multiple operators per owner.
+    /// param _operator Address to add to the set of authorized operators
+    /// param _approved True if the operator is approved, false to revoke approval
+    //function setApprovalForAll(address _operator, bool _approved) external;
+
+    /// notice Get the approved address for a single NFT
+    /// dev Throws if `_tokenId` is not a valid NFT.
+    /// param _tokenId The NFT to find the approved address for
+    /// return The approved address for this NFT, or the zero address if there is none
+    //function getApproved(uint256 _tokenId) external view returns (address);
+
+    /// notice Query if an address is an authorized operator for another address
+    /// param _owner The address that owns the NFTs
+    /// param _operator The address that acts on behalf of the owner
+    /// return True if `_operator` is an approved operator for `_owner`, false otherwise
+    //function isApprovedForAll(address _owner, address _operator) external view returns (bool);
+
+
+    //interface ERC165 {
+    /// notice Query if a contract implements an interface
+    /// param interfaceID The interface identifier, as specified in ERC-165
+    /// dev Interface identification is specified in ERC-165. This function
+    ///  uses less than 30,000 gas.
+    /// return `true` if the contract implements `interfaceID` and
+    ///  `interfaceID` is not 0xffffffff, `false` otherwise
+    //function supportsInterface(bytes4 interfaceID) external view returns (bool);
+
+    /// dev Note: the ERC-165 identifier for this interface is 0x150b7a02.
+    //interface ERC721TokenReceiver {
+    /// notice Handle the receipt of an NFT
+    /// dev The ERC721 smart contract calls this function on the recipient
+    ///  after a `transfer`. This function MAY throw to revert and reject the
+    ///  transfer. Return of other than the magic value MUST result in the
+    ///  transaction being reverted.
+    ///  Note: the contract address is always the message sender.
+    /// param _operator The address which called `safeTransferFrom` function
+    /// param _from The address which previously owned the token
+    /// param _tokenId The NFT identifier which is being transferred
+    /// param _data Additional data with no specified format
+    /// return `bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"))`
+    ///  unless throwing
+    //function onERC721Received(address _operator, address _from, uint256 _tokenId, bytes _data) external returns(bytes4);
+
+
+
 
     // ERC721 Metadata
     /// title ERC-721 Non-Fungible Token Standard, optional metadata extension

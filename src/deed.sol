0b7883531eb6b5135bd74d2c0abddde14a4e89d5
Brian McMichael <brian@brianmcmichael.com>
2020-12-10 23:00:52 -0500

    rm onERC721Received

--- src/deed.sol
+++ src/deed.sol
@@ -96,10 +96,6 @@ contract DSDeed is ERC721, ERC721Enumerable, ERC721Metadata, DSAuth {
         return _usrDeeds[guy][idx];
     }
 
-    function onERC721Received(address, address, uint256, bytes calldata) external override returns(bytes4) {
-        return this.onERC721Received.selector;
-    }
-
     function _isContract(address addr) private view returns (bool) {
         bytes32 codehash;
         bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470; // EIP-1052
